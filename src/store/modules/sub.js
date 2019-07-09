abf8a29568d07c109ca31ef27932270ba50f2077
KwonYG <andamirocll@gmail.com>
2019-07-09 17:08:37 +0900

    Refactoring: FormSection.vue, Get subscription result in Vuex Actions and use alert type with Vuex state value

--- src/store/modules/sub.js
+++ src/store/modules/sub.js
@@ -4,7 +4,10 @@ export const subModule = {
   state: {
     subResult: {
       isSub: false,
-      isCertify: false
+      isCertify: false,
+      message: "",
+      userState: "",
+      alertType: ""
     }
   },
 
@@ -16,6 +19,17 @@ export const subModule = {
 
   mutations: {
     SET_SUB_RESULT(state, subData) {
+      if (subData.isSub === true && subData.isCertify === true) {
+        subData.userState = "구독 중";
+        subData.alertType = "success";
+      } else if (subData.isCertify === false && subData.isSub === true) {
+        subData.userState = "미인증 구독자";
+        subData.alertType = "error";
+      } else if (subData.isCertify === false) {
+        subData.userState = "구독 신청 완료!";
+        subData.alertType = "success";
+      }
+
       state.subResult = subData;
     }
   },
@@ -27,8 +41,7 @@ export const subModule = {
         .then(({ data }) => {
           commit("SET_SUB_RESULT", data);
         })
-        .catch(err => {
-        });
+        .catch(err => {});
     }
   }
 };
