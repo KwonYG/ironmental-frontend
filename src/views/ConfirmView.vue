51786c9469e69112f51255da69f43bd7efcd89de
KwonYG <andamirocll@gmail.com>
2019-07-09 17:48:53 +0900

    Refactoring: ConfirmView.vue, move logic to vuex's authModule

--- src/views/ConfirmView.vue
+++ src/views/ConfirmView.vue
@@ -1,89 +1,64 @@
 <template>
-<section class="confirm_section" id="confirm_section_id">
+  <section class="confirm_section" id="confirm_section_id">
     <div class="confirm_container">
-        <div class="section_intro"> 
-            <mdb-icon v-if="isError" icon="exclamation-circle" class="error_icon" size="5x"/>
-            <h2 class="intro_title">
-                {{ title }} 
-            </h2>
-            <p class="intro_content">{{ message }}</p>
-        </div>
-        <div class="content_box">
-
-        </div>
+      <div class="section_intro">
+        <mdb-icon v-if="authResult.isError" icon="exclamation-circle" class="error_icon" size="5x" />
+        <h2 class="intro_title">{{ authResult.title }}</h2>
+        <p class="intro_content">{{ authResult.message }}</p>
+      </div>
+      <div class="content_box"></div>
     </div>
-</section>
+  </section>
 </template>
 
 <script>
-import { mdbIcon } from 'mdbvue';
+import { mapGetters } from "vuex";
+import { mdbIcon } from "mdbvue";
 export default {
-    name: "confirmView",
-    components:{
-        mdbIcon
-    },
-    
-    data(){
-        return{
-            isError: false,
-            title:'',
-            message:'',
-        }
-    },
+  name: "confirmView",
+  components: {
+    mdbIcon
+  },
 
-    created(){
-        const confirmCode = this.$route.params.confirmCode;
-        
-        this.$store.dispatch('REQUEST_AUTH',{ confirmCode })
-        .then(()=>{
-            this.title = '인증이 완료되었습니다!';
-            this.message = '매주 이메일을 통해 인터뷰를 보내드립니다.';
-        })
-        .catch(err => {
-            this.isError = true;
-            const errorCode = err.response.status;
-            
-            if(errorCode === 400){ // 형식이 잘못된 인증코드
-                this.title = errorCode;
-                this.message = '잘못된 인증코드입니다.';
-            } else if(errorCode === 404){ // 형식은 맞지만 구독신청이 안되어있는 경우
-                this.title = errorCode;
-                this.message = '잘못된 인증코드입니다.';
-            } else if(errorCode === 500){ // 서버오류
-                this.title = '서버 오류';
-                this.message='개발자의 멘탈이 나간 상태입니다. 조금만 기다려주세요.';
-            }
-        });
-    }
+  computed: {
+    ...mapGetters({
+      authResult: "fetchedAuthResult"
+    })
+  },
+
+  created() {
+    const confirmCode = this.$route.params.confirmCode;
+    this.$store.dispatch("REQUEST_AUTH", { confirmCode });
+  }
 };
 </script>
 
 <style scoped>
-.confirm_section{
-    position: relative;
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    height: 800px;
-    background: linear-gradient( to bottom, #006EF0, #00E1F0 );
-    color:#FFF;
-    text-align: center;
+.confirm_section {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 800px;
+  background: linear-gradient(to bottom, #006ef0, #00e1f0);
+  color: #fff;
+  text-align: center;
 }
 
-.error_icon{
-    margin-bottom: 10px;
+.error_icon {
+  margin-bottom: 10px;
 }
 
-.section_intro .intro_title{
-    font-weight: 800;
+.section_intro .intro_title {
+  font-weight: 800;
 }
 
-.content_box{
-    width: 500px;
-    margin: 50px auto;
+.content_box {
+  width: 500px;
+  margin: 50px auto;
 }
 
-.content_box form{
-    display:flex;
+.content_box form {
+  display: flex;
 }
 </style>
