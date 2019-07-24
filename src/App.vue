a9a4852e1c3a47c967351f9285086ff0254deb36
KwonYG <andamirocll@gmail.com>
2019-07-25 02:50:41 +0900

    Update: routes/index.js, Chane dynamic import to static import

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,70 @@
 <template>
-  <div id="app">
-    <nav-bar></nav-bar>
-    <router-view />
-    <footer-bar></footer-bar>
-  </div>
+	<div id="app">
+		<nav-bar></nav-bar>
+		<router-view />
+		<footer-bar></footer-bar>
+	</div>
 </template>
 <script>
-import NavBar from "./components/NavBar.vue";
-import FooterBar from "./components/FooterBar.vue";
-import Prism from "prismjs";
-import bus from "./utils/bus.js";
+import NavBar from './components/NavBar.vue';
+import FooterBar from './components/FooterBar.vue';
+import Prism from 'prismjs';
+import bus from './utils/bus.js';
 
 export default {
-  components: {
-    NavBar,
-    FooterBar
-  },
+	components: {
+		NavBar,
+		FooterBar,
+	},
 
-  methods: {
-    highlightCode() {
-      Prism.highlightAll();
-    }
-  },
+	methods: {
+		highlightCode() {
+			Prism.highlightAll();
+		},
+	},
 
-  created() {
-    bus.$on("execute:highlight", this.highlightCode);
-  },
+	created() {
+		bus.$on('execute:highlight', this.highlightCode);
+	},
 
-  beforeDestroy() {
-    bus.$off("execute:highlight", this.highlightCode);
-  }
+	beforeDestroy() {
+		bus.$off('execute:highlight', this.highlightCode);
+	},
 };
 </script>
 <style>
 #app {
-  font-family: "Noto Sans KR", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+	font-family: 'Noto Sans KR', sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
 }
 
 /* Extra small devices (portrait phones, less than 576px) */
 @media (max-width: 575.98px) {
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    font-size: 1.3rem;
-    font-weight: bolder;
-  }
-  .content_box {
-    font-size: 0.9rem;
-  }
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6 {
+		font-size: 1.3rem;
+		font-weight: bolder;
+	}
+	.content_box {
+		font-size: 0.9rem;
+	}
 }
 
 /* Small devices (landscape phones, 576px and up) */
 @media (min-width: 576px) and (max-width: 767.98px) {
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    font-size: 1.3rem;
-  }
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6 {
+		font-size: 1.3rem;
+	}
 }
 
 /* Medium devices (tablets, 768px and up) */
